98615387916b7673eed769ab322316fda5b2a386
Pedro Sperandio <126828311+pedroluc4as@users.noreply.github.com>
2023-08-03 16:12:02 -0400

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, unused_local_variable, avoid_print
+// ignore_for_file: prefer_const_constructors, unused_local_variable, avoid_print, sort_child_properties_last, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -12,35 +12,187 @@ class PaginaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      routes: {
+        '/': (context) => Home(),
+        '/contador': (context) => Contador(),
+        '/curtir': (context) => Curtir(),
+      },
+      theme: ThemeData.dark(),
     );
   }
 }
+
+class Curtir extends StatelessWidget {
+  const Curtir({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("LIKE"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            IconButton(
+              iconSize: 100,
+              icon: Icon(Icons.favorite_outline,
+                  color: Color.fromARGB(255, 255, 0, 0)),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    String x = "100";
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Meu Aplicativo"),
+        backgroundColor: Colors.black,
+        title: Text("COUNTER and LIKES"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.exposure_plus_1,
+              size: 36,
+              color: Color.fromARGB(255, 136, 255, 0),
+            ),
+            title: Text("Counter"),
+            subtitle: Text("Increment example"),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/contador');
+            },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.favorite,
+              size: 36,
+              color: Color.fromARGB(255, 255, 0, 0),
+            ),
+            title: Text("Like"),
+            subtitle: Text(""),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {
+              Navigator.pushNamed(context, '/curtir');
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Contador extends StatefulWidget {
+  const Contador({super.key});
+
+  @override
+  State<Contador> createState() => _ContadorState();
+}
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class _ContadorState extends State<Contador> {
+  int x = 100;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        title: Text("COUNTER"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(x),
+            Text(
+              x.toString(),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Montserrat',
+                  fontSize: 70,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      x = x + 1;
+                    });
+                  },
+                  child: Text("+1"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(0, 0, 0, 0)),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      x = x - 1;
+                    });
+                  },
+                  child: Text("-1"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(0, 0, 0, 0)),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      x = x + 100;
+                    });
+                  },
+                  child: Text("+100"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 0, 255, 0)),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      x = x - 100;
+                    });
+                  },
+                  child: Text("-100"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 255, 0, 0)),
+                ),
+              ],
+            ),
             ElevatedButton(
-                onPressed: () {
-                  print("Antes de Zerar");
-                  print(x);
-                  x = "0";
-                  print("Depois de Zerar");
-                  print(x);
-                },
-                child: Text("Zerar")),
+              onPressed: () {
+                setState(() {
+                  x = 0;
+                });
+              },
+              child: Text("RESET"),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(0, 0, 0, 0)),
+            ),
           ],
         ),
       ),
