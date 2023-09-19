a30c36717770aa1c670156037e69d32445e2193b
Pedro Sperandio <126828311+pedroluc4as@users.noreply.github.com>
2023-09-19 14:28:40 -0400

    Add files via upload

--- lib/like.dart
+++ lib/like.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, unused_local_variable, avoid_print, sort_child_properties_last, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Curtir extends StatefulWidget {
   const Curtir({super.key});
@@ -14,7 +14,30 @@ class _CurtirState extends State<Curtir> {
   int x = 0;
 
   @override
-  Widget build(BuildContext context) {
+  void initState() { //roda na hora que abre
+    super.initState();
+    obtemValor(); //lê da memória hora que abre 
+  }
+
+  void obtemValor() async {
+    //busca um valor da memória persistente
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      x = prefs.getInt('counter') ?? 0;
+      curtiu = prefs.getBool('curtiu') ?? false;
+    });
+    
+  }
+
+  void salvaValor(int valor, bool curt) async {
+    //salva um valor na mémoria persistente
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setInt('counter', valor);
+        prefs.setBool('curtiu', curt);
+  }
+
+  @override
+   build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
@@ -36,14 +59,15 @@ class _CurtirState extends State<Curtir> {
             IconButton(
               iconSize: 100,
               icon: curtiu == true 
-              ? Icon(Icons.favorite, color: Colors.red,)
-              : Icon(Icons.favorite_outline, color: Colors.white),
+              ? Icon(Icons.favorite, color: Colors.red,)//?= IF (SE O VALOR FOR VERDADEIRO)
+              : Icon(Icons.favorite_outline, color: Colors.white),//:= ELSE(SE O VALOR FOR)
                   color: curtiu == true ? Colors.red : Colors.white,
               onPressed: () {
                 setState(() {
                   curtiu = true;
                   x = x + 1;
                 });
+                salvaValor(x, curtiu);
               },
             ),
           ],
